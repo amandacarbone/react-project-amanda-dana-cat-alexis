ee436b1a98184c9a35b19654adb26f6b30ee91a5
Amanda Carbone <amandamooney0418@gmail.com>
2022-02-17 16:30:13 -0500

    FAVORITES DISPLAYS RELEVANT INFORMATION

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -1,33 +1,35 @@
 import { useContext } from "react";
 import FavoritesContext from "../contexts/FavoritesContext";
-import { FavoritesItem } from "./FavoritesItem";
-import { ProfileItem } from "./ProfileItem";
 
 export function Favorites() {
 
   const { favoriteDogs, removeFavorite } = useContext(FavoritesContext);
 
-  function showRemoveButton() {}
+  function showRemoveButton() {
+    let foundItem = favoriteDogs.find((dog) => dog.id)
+    if(foundItem) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
   return (
     <div>
       <h2>Favorites</h2>
 
-      {/* {favoriteDogs.map((dog) => {
-        <ul key={dog.id}>
-          <li>{dog.name}</li>
-        </ul>
-      })} */}
-
-      <ul>
         {favoriteDogs.map((dog) => (
-          <ProfileItem 
-          handleRightClick={() =>  {}} 
-          handleLeftClick={() =>  {}} 
-          key={dog.id} 
-          dog={dog}/>
+          <ul key={dog.id}>
+          <li>{dog.name}</li>
+          <li><img src={
+            dog?.primary_photo_cropped?.small &&
+            dog?.primary_photo_cropped?.medium &&
+            dog?.primary_photo_cropped?.large}/>
+          </li>
+          <button className={showRemoveButton() === true ? "material-icons" : "hide"} 
+                  onClick={() => removeFavorite(dog.id)}>cancel</button>
+          </ul>
         ))}
-      </ul>
     </div>
   );
 }
