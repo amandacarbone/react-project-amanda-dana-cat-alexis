d49e4d60c03d9d18a3cf047f6f2cb5b9bdf09322
Amanda Carbone <amandamooney0418@gmail.com>
2022-02-17 12:42:49 -0500

    favorites can be added. just need to get specific information to show rather than the entire profile item element

--- src/components/ProfileItem.tsx
+++ src/components/ProfileItem.tsx
@@ -18,66 +18,22 @@ export function ProfileItem(props: Props) {
     setDog(props.dog);
   }, [props]);
 
-  // useEffect(() => {
-  //   getAnimalsByType("dog").then((newDogs: Animals[]) => {
-  //     console.log(newDogs);
-  //     setDogs(newDogs);
-  //     setSelectedDog(newDogs[0]);
-  //     console.log(newDogs);
-  //   });
-
-  //   if (dogs && dogs[0]) {
-  //     setSelectedDogIndex(0);
-  //     setSelectedDog(dogs[0]);
-  //   }
-  // }, []);
-
-  const handleRightClick = () => {
-    // if (dogs && dogs.length > 0) {
-    //   let newIndex = selectedDogIndex + 1;
-    //   console.log(newIndex, dogs.length);
-    //   if (newIndex >= dogs.length) {
-    //     newIndex = 0;
-    //   }
-    //   setSelectedDogIndex(newIndex);
-    //   console.log("after setSelected", newIndex);
-    //   setSelectedDog(dogs[newIndex]);
-    //   console.log(selectedDogIndex);
-    // }
-  };
-
-  const handleLeftClick = () => {
-    // if (dogs && dogs.length > 0) {
-    //   let newIndex = selectedDogIndex - 1;
-    //   if (newIndex < 0) {
-    //     newIndex = dogs.length - 1;
-    //   }
-    //   setSelectedDogIndex(newIndex);
-    //   setSelectedDog(dogs[newIndex]);
-    //   console.log(selectedDogIndex);
-    // }
-  };
-
   return (
     <div className="carousel-container">
       <h2 className="name">{dog?.name}</h2>
       <div className="selected-image">
-        <img
-          src={
+        <img src={
             dog?.primary_photo_cropped?.small &&
             dog?.primary_photo_cropped?.medium &&
             dog?.primary_photo_cropped?.large
-          }
-        />
+          }/>
         <div className="doggyDetails" key={dog?.id}>
           <p>Age: {dog?.age}</p>
           <p>Size: {dog?.size}</p>
           <p>Gender: {dog?.gender}</p>
           <p>Fixed: {dog?.attributes.spayed_neutered ? "Yes" : "No"}</p>
           <p>Shots Current: {dog?.attributes.shots_current ? "Yes" : "No"}</p>
-          <p>
-            Good With Children: {dog?.environment.children ? "Yes" : "No"}
-          </p>
+          <p>Good With Children: {dog?.environment.children ? "Yes" : "No"}</p>
           <p>Description: {dog?.description}</p>
         </div>
         <div className="carousel">
