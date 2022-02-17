ad3876206df3090c4b269e659d5adf1197136587
CD Rivers <riverscd90@gmail.com>
2022-02-17 10:40:21 -0500

    basic placeholder  styling for search results

--- src/components/SearchResultItem.tsx
+++ src/components/SearchResultItem.tsx
@@ -1,5 +1,5 @@
 import { Animals } from "../models/Animals";
-
+import "../styles/SearchResultItem.css"
 
 //child of SearchResults
 
@@ -7,19 +7,27 @@ interface AnimalsProp {
     dog: Animals
 }
 
-export function SearchResultItem({dog}: AnimalsProp){
+export function SearchResultItem({ dog }: AnimalsProp) {
 
-return(
-    <div>
-        {dog.name}  
-        {dog.type}
-        <img
-          src={
-            dog?.primary_photo_cropped?.small &&
-            dog?.primary_photo_cropped?.medium &&
-            dog?.primary_photo_cropped?.large
-          }
-        />
-    </div>
-)
+    return (
+        <div>
+
+            <div className="searchedDog">
+                <div className="searchedDogName">
+                    {dog.name}
+                </div>
+
+                <div >
+                    <img className="searchedDogPic"
+                        src={
+                            dog?.primary_photo_cropped?.small &&
+                            dog?.primary_photo_cropped?.medium &&
+                            dog?.primary_photo_cropped?.large
+                        }
+                    />
+                </div>
+            </div>
+
+        </div>
+    )
 }
