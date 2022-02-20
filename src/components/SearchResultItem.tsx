bce6924115d026eded8f2e978b9cb96579361dc5
a-brown77 <a.brown7795@gmail.com>
2022-02-20 12:36:55 -0500

    fixed styling in search

--- src/components/SearchResultItem.tsx
+++ src/components/SearchResultItem.tsx
@@ -1,47 +1,49 @@
 import { Link } from "react-router-dom";
 import { Animals } from "../models/Animals";
-import "../styles/SearchResultItem.css"
+import "../styles/SearchResultItem.css";
 
 //child of SearchResults
 
 interface AnimalsProp {
-    dog: Animals
+  dog: Animals;
 }
 
 export function SearchResultItem({ dog }: AnimalsProp) {
+  const styles = {
+    Link: {
+      textDecoration: "none",
+    },
+    searchedDogName: {
+      fontSize:
+        dog.name.length < 8 ? "30px" : dog.name.length < 12 ? "20px" : "15px",
+    },
+  };
+  return (
+    <Link to={`/profile/${dog.id}`} style={styles.Link}>
+      <div className="searchedDogComponent">
+        <div className="searchedDog">
+          <img
+            className="searchedDogPic"
+            src={
+              dog?.primary_photo_cropped?.small &&
+              dog?.primary_photo_cropped?.medium &&
+              dog?.primary_photo_cropped?.large
+            }
+          />
 
-  
-
-    return (
-
-        <Link to = {`/profile/${dog.id}`}>
-        <div className="searchedDogComponent">
-
-            <div className="searchedDog">
-
-               
-                    <img className="searchedDogPic"
-                        src={
-                            dog?.primary_photo_cropped?.small &&
-                            dog?.primary_photo_cropped?.medium &&
-                            dog?.primary_photo_cropped?.large
-                        }
-                    />
-                
-                <div className = "searchedDogInfo">
-                <div className="searchedDogName">
-                    {dog.name}
-                </div>
-
-                <div className="searchedDogDetails">
-                    <p>{dog.gender} | {dog.age} | {dog.size}</p>
-                </div>
-                </div>
-
-
+          <div className="searchedDogInfo">
+            <div style={styles.searchedDogName} className="searchedDogName">
+              {dog.name}
             </div>
 
+            <div className="searchedDogDetails">
+              <p>
+                {dog.gender} | {dog.age} | {dog.size}
+              </p>
+            </div>
+          </div>
         </div>
-        </Link>
-    )
+      </div>
+    </Link>
+  );
 }
