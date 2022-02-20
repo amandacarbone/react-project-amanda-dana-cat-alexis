48527c4c7a7dcace62981fecfa1721f81605a0ea
a-brown77 <a.brown7795@gmail.com>
2022-02-20 12:55:28 -0500

    no longer adds multiples of the same favorite

--- src/contexts/FavoritesContextProvider.tsx
+++ src/contexts/FavoritesContextProvider.tsx
@@ -2,35 +2,39 @@ import { ReactNode, useState } from "react";
 import { Animals } from "../models/Animals";
 import FavoritesContext from "./FavoritesContext";
 
-export function FavoritesContextProvider(props: { children: ReactNode }){
+export function FavoritesContextProvider(props: { children: ReactNode }) {
+  const [favoriteDogs, setFavoriteDogs] = useState<Animals[]>([]);
 
-    const [favoriteDogs, setFavoriteDogs] = useState<Animals[]>([]);
-
-    function addFavorite(favoriteDog: Animals) {
-        setFavoriteDogs((prev)=>[...prev, favoriteDog]);
+  function addFavorite(favoriteDog: Animals) {
+    if (
+      !favoriteDogs.some((addedDog: Animals): boolean => {
+        return addedDog.id === favoriteDog.id;
+      })
+    ) {
+      setFavoriteDogs((prev) => [...prev, favoriteDog]);
     }
+  }
 
-    function removeFavorite(id: number) {
-        setFavoriteDogs(favoriteDogs.filter(f => f.id != id));
-        // != should be !== but doing that breaks it so it's a bug but also...
-        //line 14 is not written immutably which is a problem but the code Andrea suggested:
-            //const index = favoriteDogs.findIndex((dog)=>dog.id===id)
-            //setFavoriteDogs((prev)=>[...prev.slice(0,index), ...prev.slice(index + 1)])
-        //also breaks functionality so im not sure if something is missing in the code or
-        //if something is missing elsewhere and causing trouble
-        //andrea suggested we could start by checking the useEffect to investigate? but idk
-        //it's currently "working" and I don't want to break things further with so little time
-        //so im cool submitting with this bug for the deadline
-    }
+  function removeFavorite(id: number) {
+    setFavoriteDogs(favoriteDogs.filter((f) => f.id != id));
+    // != should be !== but doing that breaks it so it's a bug but also...
+    //line 14 is not written immutably which is a problem but the code Andrea suggested:
+    //const index = favoriteDogs.findIndex((dog)=>dog.id===id)
+    //setFavoriteDogs((prev)=>[...prev.slice(0,index), ...prev.slice(index + 1)])
+    //also breaks functionality so im not sure if something is missing in the code or
+    //if something is missing elsewhere and causing trouble
+    //andrea suggested we could start by checking the useEffect to investigate? but idk
+    //it's currently "working" and I don't want to break things further with so little time
+    //so im cool submitting with this bug for the deadline
+  }
 
-    return(
-        <div>
-
-        <FavoritesContext.Provider value={{favoriteDogs, addFavorite, removeFavorite}}>
-            {props.children}
-        </FavoritesContext.Provider>
-
-        </div>
-    )
+  return (
+    <div>
+      <FavoritesContext.Provider
+        value={{ favoriteDogs, addFavorite, removeFavorite }}
+      >
+        {props.children}
+      </FavoritesContext.Provider>
+    </div>
+  );
 }
-
